fe81d52095401f63e4f3cce37bff3136e73d28de
kR1f-Tumka <prostoakk.bystro@mail.ru>
2026-04-13 10:06:20 +0300

    App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     sendMessage,
     sendTyping,
     sendStopTyping,
-  } = useWebSocket('ws://localhost:8080');
+  } = useWebSocket('wss://advanced-web-messenger.onrender.com');
 
   const handleLogin = (name: string) => {
     setUsername(name);
